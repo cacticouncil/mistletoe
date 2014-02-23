795fac5179aa9a52e3b54fb4d5abed379c021734
Jeremiah Blanchard <jeremiahb@fullsail.com>
2014-02-23 09:38:13 -0500

    Added README; minor adjustments to build script

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,14 @@
 """
 Usage:
-    MacOS X: python setup.py bdist_dmg
-    Windows: python setup.py bdist_msi
+    Test Build:
+        python setup.py build
+    MacOS X Distribution:
+        python setup.py bdist_dmg
+    Windows Distribution (not yet working):
+        python setup.py (Eventually will use bdist_nsi)
 """
 
-# Make sure prereqs are available
-import ez_setup
-ez_setup.use_setuptools()
-
-import sys
-from cx_Freeze import setup, Executable
-
+# Application setup variables
 NAME = 'Mistletoe'
 VERSION = '0.8'
 DESCRIPTION = 'GUI for MOSS'
@@ -21,22 +19,38 @@ MODULES = [ 'PySide.QtXml' ]
 MAC_ICON = 'mistletoe.icns'
 WIN_ICON = 'mistletoe.ico'
 
+# Make sure prereqs are available
+import ez_setup
+ez_setup.use_setuptools()
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Deal with Windows-specific requirements
 if sys.platform == 'win32':
+    import bdist_nsi
     BASE = 'Win32GUI'
 else:
     BASE = None
 
+# Prepare setup options
 EXECUTABLES = [ Executable(ENTRYPOINT, base=BASE) ]
 FREEZE_OPTIONS = {'includes': MODULES, 'include_files': DATA_FILES, 'icon': WIN_ICON, 'include_msvcr': True }
+
+NSIS_OPTIONS = {}
 BUNDLE_OPTIONS = {'iconfile': MAC_ICON, 'bundle_name': NAME }
 DMG_OPTIONS = {'volume_label': NAME }
 #DMG_OPTIONS = {'volume_label': NAME, 'applications-shortcut': True }
 
+# Perform application setup (execute command)
 setup(
-    app=[ENTRYPOINT],
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    options = {'build_exe': FREEZE_OPTIONS, 'bdist_mac': BUNDLE_OPTIONS, 'bdist_dmg': DMG_OPTIONS },
-    executables = EXECUTABLES
+    executables = EXECUTABLES,
+    options =
+    {
+        'build_exe': FREEZE_OPTIONS, 'bdist_nsi': NSIS_OPTIONS,
+        'bdist_mac': BUNDLE_OPTIONS, 'bdist_dmg': DMG_OPTIONS
+    }
 )
